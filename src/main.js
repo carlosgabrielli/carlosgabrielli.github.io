006b8ac7267f59078883054451e31105a47ce5ba
Carlosperdomo7 <50246757+Carlosperdomo7@users.noreply.github.com>
2019-08-21 11:05:57 -0300

    import firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
@@ -8,3 +9,15 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAm597FCzyDQu-lNvtcE79LhzI3uFeBY2A",
+  authDomain: "tesis-b214c.firebaseapp.com",
+  databaseURL: "https://tesis-b214c.firebaseio.com",
+  projectId: "tesis-b214c",
+  storageBucket: "",
+  messagingSenderId: "634526123704",
+  appId: "1:634526123704:web:73558123f4b961f3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
