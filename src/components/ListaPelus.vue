b48cc22a15f38e2b8bd42cf6403d969cefb864a9
Carlosperdomo7 <50246757+Carlosperdomo7@users.noreply.github.com>
2019-10-21 23:16:59 -0300

    todo

---
+++ src/components/ListaPelus.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-toolbar
+      class="px-4"
+      color="transparent"
+      flat
+      
+    >
+      <v-text-field
+        hide-details
+        single-line
+        outlined
+        rounded
+        class="mr-4"
+      ></v-text-field>
+
+      <v-btn class="primary" icon>
+        <v-icon>mdi-magnify mdi-light</v-icon>
+      </v-btn>
+    </v-toolbar>
+  
+</template>
