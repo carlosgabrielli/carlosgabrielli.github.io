eaa4c04a33fa911eea34b830c36b41303b41e684
lautaro903 <lautaroalmada903@gmail.com>
2019-10-22 02:53:41 -0300

    Pantalla inicio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -67,13 +67,57 @@
         </v-slide-group>
       </v-col>
     </v-row>
+    <v-card class="mx-auto p-0 mb-4" max-width="380" outlined>
+      <v-list-item three-line>
+        <v-list-item-avatar tile size="70" color="grey"></v-list-item-avatar>
+        <v-btn icon class="btn-heart">
+          <v-icon size="16">mdi-heart-outline</v-icon>
+        </v-btn>
+        <v-list-item-content class="mr-4 ml-n2">
+          <v-list-item-title
+            class="headline mb-1 subtitle-1 font-weight-bold"
+          >El club de la milanesa</v-list-item-title>
+          <v-list-item-subtitle class="subtitle-2 font-weight-regular">Ensaladas, Hamburguesas</v-list-item-subtitle>
+          <v-card-subtitle class="caption pa-0">Av. Hipolito Yrigoyen 320 - 2km</v-card-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+        <v-card class="mx-auto p-0 mb-4" max-width="380" outlined>
+      <v-list-item three-line>
+        <v-list-item-avatar tile size="70" color="grey"></v-list-item-avatar>
+        <v-btn icon class="btn-heart">
+          <v-icon size="16">mdi-heart-outline</v-icon>
+        </v-btn>
+        <v-list-item-content class="mr-4 ml-n2">
+          <v-list-item-title
+            class="headline mb-1 subtitle-1 font-weight-bold"
+          >El club de la milanesa</v-list-item-title>
+          <v-list-item-subtitle class="subtitle-2 font-weight-regular">Ensaladas, Hamburguesas</v-list-item-subtitle>
+          <v-card-subtitle class="caption pa-0">Av. Hipolito Yrigoyen 320 - 2km</v-card-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+        <v-card class="mx-auto p-0 mb-4" max-width="380" outlined>
+      <v-list-item three-line>
+        <v-list-item-avatar tile size="70" color="grey"></v-list-item-avatar>
+        <v-btn icon class="btn-heart">
+          <v-icon size="16">mdi-heart-outline</v-icon>
+        </v-btn>
+        <v-list-item-content class="mr-4 ml-n2">
+          <v-list-item-title
+            class="headline mb-1 subtitle-1 font-weight-bold"
+          >El club de la milanesa</v-list-item-title>
+          <v-list-item-subtitle class="subtitle-2 font-weight-regular">Ensaladas, Hamburguesas</v-list-item-subtitle>
+          <v-card-subtitle class="caption pa-0">Av. Hipolito Yrigoyen 320 - 2km</v-card-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
   </v-container>
   
 </template>
 <script>
  export default {
-    data: () => ({
-
-    }),
-  } 
+  name: "Home",
+  components: {}
+};
 </script>
