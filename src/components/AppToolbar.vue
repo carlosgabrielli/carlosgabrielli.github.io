b0ef4eda0a51f4bc0fb504a9cb03989d923904fa
Carlos Gabrielli <carlosagustingabrielli@gmail.com>
2019-10-23 00:24:14 -0300

    Nuevos estilos y vista de usuario

--- src/components/AppToolbar.vue
+++ src/components/AppToolbar.vue
@@ -1,28 +1,38 @@
 <template>
   <div>
     <v-dialog v-model="dialog" hide-overlay transition="dialog-bottom-transition" fullscreen>
-      <v-card>
-        <v-toolbar dark color="primary">
-          <v-btn icon dark @click="dialog = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-          <v-toolbar-title>Mi cuenta</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-toolbar-items></v-toolbar-items>
-        </v-toolbar>
-        <v-card-title v-if="user">{{ user.displayName }}</v-card-title>
-        <v-card-subtitle>{{user.email}}</v-card-subtitle>
-        <v-row justify="center">
-          <v-avatar class="profile" color="grey" size="120">
+      <v-toolbar fixed-header dark color="primary" height="40px">
+        <v-btn icon dark @click="dialog = false">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card class="mx-auto box-shadow-none border-0" max-width="400">
+        <div class="bg-primary white--text align-end border-radius-bx text-center">
+          <v-avatar class="profile" color="grey" size="100">
             <v-img :src="user.photoURL" width="100"></v-img>
           </v-avatar>
-        </v-row>
+          <v-card-title class="d-flex justify-center" v-if="user">{{ user.displayName }}</v-card-title>
+        </div>
+        <v-card-text class="text--primary pa-0">
+          <v-form>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6">
+                  <h3 class="mb-5">Mis datos</h3>
+                  <v-text-field label="Nombre" outlined></v-text-field>
+                  <v-text-field label="Apellido" outlined></v-text-field>
+                  <div class="pb-5">
+                    <a href>Cambiar contraseña</a>
+                  </div>
+                  <h3 class="border-top pt-5">Mis pedidos</h3>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-card-text>
         <v-card-actions>
-          <v-spacer></v-spacer>
-
-          <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-
-          <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+          <v-btn color="orange" text>Share</v-btn>
+          <v-btn color="orange" text>Explore</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
