2f12295ff1ee26a434c39ebd77c5602f2d505812
Leagerosa <carlosagustingabrielli@gmail.com>
2019-10-23 12:46:56 -0300

    Bares y locales

--- src/views/BaresRestaurantes.vue
+++ src/views/BaresRestaurantes.vue
@@ -71,78 +71,7 @@
                 </v-slide-group>
             </v-col>
         </v-row>
-        <v-row>
-            <v-col>
-                <v-card class="mb-4" max-width="380" outlined>
-                <v-list-item three-line>
-                    <v-list-item-avatar tile size="70" color="grey lighten-5">
-                        <v-img class="mr-2" src="../assets/img/LogoJohnny.png"></v-img>
-                    </v-list-item-avatar>
-                    <v-btn icon class="btn-heart">
-                    <v-icon size="16">mdi-heart-outline</v-icon>
-                    </v-btn>
-                    <v-list-item-content class="mr-4 ml-n2">
-                    <v-list-item-title
-                        class="headline mb-1 subtitle-1 font-weight-bold"
-                    >El club de la milanesa</v-list-item-title>
-                    <v-list-item-subtitle class="subtitle-2 font-weight-regular">Ensaladas, Hamburguesas</v-list-item-subtitle>
-                    <v-card-subtitle class="caption pa-0">Av. Hipolito Yrigoyen 320 - 2km</v-card-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-                </v-card>
-            </v-col>
-                <v-col>
-                <v-card class="mb-4" max-width="380" outlined>
-                <v-list-item three-line>
-                    <v-list-item-avatar tile size="70" color="grey"></v-list-item-avatar>
-                    <v-btn icon class="btn-heart">
-                    <v-icon size="16">mdi-heart-outline</v-icon>
-                    </v-btn>
-                    <v-list-item-content class="mr-4 ml-n2">
-                    <v-list-item-title
-                        class="headline mb-1 subtitle-1 font-weight-bold"
-                    >El club de la milanesa</v-list-item-title>
-                    <v-list-item-subtitle class="subtitle-2 font-weight-regular">Ensaladas, Hamburguesas</v-list-item-subtitle>
-                    <v-card-subtitle class="caption pa-0">Av. Hipolito Yrigoyen 320 - 2km</v-card-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-                </v-card>
-            </v-col>
-                <v-col>
-                <v-card class="mb-4" max-width="380" outlined>
-                <v-list-item three-line>
-                    <v-list-item-avatar tile size="70" color="grey"></v-list-item-avatar>
-                    <v-btn icon class="btn-heart">
-                    <v-icon size="16">mdi-heart-outline</v-icon>
-                    </v-btn>
-                    <v-list-item-content class="mr-4 ml-n2">
-                    <v-list-item-title
-                        class="headline mb-1 subtitle-1 font-weight-bold"
-                    >El club de la milanesa</v-list-item-title>
-                    <v-list-item-subtitle class="subtitle-2 font-weight-regular">Ensaladas, Hamburguesas</v-list-item-subtitle>
-                    <v-card-subtitle class="caption pa-0">Av. Hipolito Yrigoyen 320 - 2km</v-card-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-                </v-card>
-            </v-col>
-                <v-col>
-                <v-card class="mb-4" max-width="380" outlined>
-                <v-list-item three-line>
-                    <v-list-item-avatar tile size="70" color="grey"></v-list-item-avatar>
-                    <v-btn icon class="btn-heart">
-                    <v-icon size="16">mdi-heart-outline</v-icon>
-                    </v-btn>
-                    <v-list-item-content class="mr-4 ml-n2">
-                    <v-list-item-title
-                        class="headline mb-1 subtitle-1 font-weight-bold"
-                    >El club de la milanesa</v-list-item-title>
-                    <v-list-item-subtitle class="subtitle-2 font-weight-regular">Ensaladas, Hamburguesas</v-list-item-subtitle>
-                    <v-card-subtitle class="caption pa-0">Av. Hipolito Yrigoyen 320 - 2km</v-card-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-                </v-card>
-            </v-col>
-        </v-row>
+        
     </v-container>
 </template>
 <script>
