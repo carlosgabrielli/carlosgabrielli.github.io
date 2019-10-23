c9c808ea2d833bc75854096d7d7574f95a54668b
lautaro903 <lautaroalmada903@gmail.com>
2019-10-23 10:18:51 -0300

    Card Categorias. Buscador. Boton filtro

--- src/views/BaresRestaurantes.vue
+++ src/views/BaresRestaurantes.vue
@@ -2,7 +2,7 @@
     <v-container>
         <v-row>
             <v-col>
-                <v-icon>fas fa-edit</v-icon>
+                <v-icon>mdi-chevron-left</v-icon>
             </v-col>
         </v-row>
         <v-row>
@@ -11,7 +11,7 @@
             </v-col>
         </v-row>
         <v-row>
-            <v-col cols="12" sm="6">
+            <v-col cols="11" sm="6">
                 <v-text-field
                     v-model="message3"
                     filled
@@ -19,25 +19,56 @@
                     clearable
                 ></v-text-field>
             </v-col>
-            <v-col>
-                <v-icon>fas fa-edit</v-icon>
-            </v-col>
+                <v-icon class="mb-5">mdi-playlist-edit</v-icon>
         </v-row>
-        <v-row>
+            <h2 class="title">Promociones</h2>
+            <v-row>
             <v-col>
                 <v-slide-group>
-                <v-slide-item>
-                    <v-img class="mr-2" src="../assets/img/promoHamburguesa.jpg"></v-img>
-                </v-slide-item>
-                <v-slide-item>
-                    <v-img class="ml-2" src="../assets/img/promoCerveza.jpg"></v-img>
-                </v-slide-item>
+                    <v-slide-item>
+                        <v-img class="mr-2" src="../assets/img/promoHamburguesa.jpg"></v-img>
+                    </v-slide-item>
+                    <v-slide-item>
+                        <v-img class="ml-2" src="../assets/img/promoCerveza.jpg"></v-img>
+                    </v-slide-item>
                 </v-slide-group>
             </v-col>
-        </v-row>
+            </v-row>
         <v-row>
             <v-col>
                 <h2 class="title">Categorias</h2>
+            </v-col>
+            <v-col>
+                <v-slide-group class="mt-10">
+                    <v-slide-item>
+                        <v-card class="mr-2" @click="toggle" max-height="90">
+                        <v-img src="../assets/img/carnes.png" class="pl-8">
+                            <v-card-title class="black--text pl-1 " >Carnes</v-card-title>
+                        </v-img>
+                        </v-card>
+                    </v-slide-item>
+                    <v-slide-item>
+                        <v-card class="mx-2" @click="toggle">
+                        <v-img src="../assets/img/ensalada.png">
+                            <v-card-title class="black--text pt-1 pl-2 f-18">Ensaladas</v-card-title>
+                        </v-img>
+                        </v-card>
+                    </v-slide-item>
+                    <v-slide-item>
+                        <v-card class="mx-2" @click="toggle">
+                        <v-img src="../assets/img/empanada.png">
+                            <v-card-title class="black--text pt-1 pl-2 f-18">Empanadas</v-card-title>
+                        </v-img>
+                        </v-card>
+                    </v-slide-item>
+                    <v-slide-item>
+                        <v-card class="mx-2" @click="toggle">
+                        <v-img src="../assets/img/hambur.png">
+                            <v-card-title class="black--text pt-1 pl-2 f-18">Hamburguesas</v-card-title>
+                        </v-img>
+                        </v-card>
+                    </v-slide-item>
+                </v-slide-group>
             </v-col>
         </v-row>
         <v-row>
