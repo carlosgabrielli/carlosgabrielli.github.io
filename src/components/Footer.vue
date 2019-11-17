e60a10a48b31a127449f9f235b6dba4e32ed46ca
Carlos Gabrielli <carlosagustingabrielli@gmail.com>
2019-11-17 18:29:43 -0300

    ! Arreglo bug modal footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,49 @@
 <template>
- <div>
-   <!-- Dialog buscar -->
-    <v-dialog v-model="dialogBuscador" hide-overlay transition="dialog-bottom-transition" fullscreen>
-      <v-card class=" box-shadow-none bg-app">
-          <v-toolbar dark color="primary" class="box-shadow-none">
-            <v-btn icon dark @click="dialogBuscador = false" absolute fab small top left class="mt-6 w-0">
-              <v-icon class="mt-2">mdi-chevron-left</v-icon> 
-              
-            </v-btn>
-            <h2 class="title ml-7">Buscador</h2>
-          </v-toolbar>
-        
-      
-        <v-row>
-          <v-col cols="11" sm="6" class="ml-4 mt-6 pb-0">
-            <v-text-field v-on:click:clear="clearFiltro" v-model="filtro" filled label="Buscar locales..." clearable></v-text-field>
-          </v-col>
-        </v-row>
-  <!--Cartas-->
-        <CardLocal/>
+  <div>
+    <!-- Dialog buscar -->
+    <v-dialog
+      v-model="dialogBuscador"
+      hide-overlay
+      transition="dialog-bottom-transition"
+      fullscreen
+    >
+      <v-card class="mx-auto box-shadow-none border-0 bg-app">
+        <v-toolbar dark color="primary" class="box-shadow-none">
+          <v-btn
+            icon
+            dark
+            @click="dialogBuscador = false"
+            absolute
+            fab
+            small
+            top
+            left
+            class="mt-6 w-0"
+          >
+            <v-icon class="mt-2">mdi-chevron-left</v-icon>
+          </v-btn>
+          <h2 class="title ml-7">Buscador</h2>
+        </v-toolbar>
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-on:click:clear="clearFiltro"
+                v-model="filtro"
+                filled
+                label="Buscar locales..."
+                clearable
+              ></v-text-field>
+            </v-col>
+          </v-row>
+                  <CardLocal />
+        </v-container>
+
+
+        <!--Cartas-->
       </v-card>
     </v-dialog>
-  <!-- Footer -->
+    <!-- Footer -->
     <v-footer app fixed style="padding:0;">
       <v-bottom-navigation grow color="teal">
         <v-btn @click="$router.push('/home')">
@@ -30,13 +52,13 @@
         <v-btn @click.stop="dialogBuscador = true">
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
-    
+
         <v-btn>
           <v-icon>mdi-clipboard-text-outline</v-icon>
         </v-btn>
       </v-bottom-navigation>
     </v-footer>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -55,17 +77,16 @@ export default {
       dialogBuscador: false,
       show: false,
       resultados: [],
-      filtro: ''
+      filtro: ""
     };
-
   },
   watch: {
     menu(val) {
       val && setTimeout(() => (this.$refs.picker.activePicker = "YEAR"));
     },
-    filtro (val) {
-      this.buscador()
-    },
+    filtro(val) {
+      this.buscador();
+    }
   },
 
   methods: {
@@ -73,11 +94,11 @@ export default {
       this.$refs.menu.save(date);
     },
     buscador() {
-      console.log(this.filtro)
-      this.$store.dispatch('locales/buscador', this.filtro)
+      console.log(this.filtro);
+      this.$store.dispatch("locales/buscador", this.filtro);
     },
-    clearFiltro () {
-      this.filtro = ''
+    clearFiltro() {
+      this.filtro = "";
     }
   }
 };
